5d8a6a13375224a8d705bbcc11b24af1a4e3ff24
Hannah Hoang <112083555+hannahhoang2704@users.noreply.github.com>
2023-02-25 16:02:56 +0200

    update scoreboard

--- Turtle_Crossing_street_game/scoreboard.py
+++ Turtle_Crossing_street_game/scoreboard.py
@@ -1,4 +1,24 @@
+from turtle import Turtle
 FONT = ("Courier", 24, 'normal')
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.setpos(-280, 250)
+        self.level_number = 1
+        self.level_status()
+
+    def level_status(self):
+        self.clear()
+        self.write(f"Level: {self.level_number}", False, align="left", font=FONT)
+
+    def lvl_increase(self):
+        self.level_number += 1
+        self.level_status()
+
+    def gameover(self):
+        self.goto(0,0)
+        self.write("GAME OVER", False, align='center', font=FONT)
+
