e862f93aae9c68dd0348e6e7ba173fac1cc7ba49
Hannah Hoang <112083555+hannahhoang2704@users.noreply.github.com>
2023-02-26 11:10:13 +0200

    Record highscore

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -9,6 +9,8 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.color('white')
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.penup()
         self.hideturtle()
         self.setpos(0,280)
@@ -17,12 +19,19 @@ class Score(Turtle):
 
     def score_status(self):
         self.clear()
-        self.write("Score: " + str(self.score), False, align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} | High score: {self.high_score}", False,align=ALIGNMENT, font=FONT)
 
-    def gameover(self):
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score_status()
+
+    """def gameover(self):
         self.goto(0,0)
-        self.write("GAME OVER", False, align=ALIGNMENT,
-                   font=FONT)
+        self.write("GAME OVER", False, align=ALIGNMENT,font=FONT)"""
+
     def score_increase(self):
         self.score += 1
         self.score_status()
